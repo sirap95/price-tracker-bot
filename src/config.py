98cc31e9735826bf5a10ed8e002ce75726081f83
gregorio siravo <gregoriosiravo@gregorios-MacBook-Pro.local>
2024-06-24 15:09:00 -0300

    added .py files for api, still WIP not working and folder structure refactor needed

---
+++ src/config.py
@@ -0,0 +1,9 @@
+BOT_TOKEN = 'ADD_YOUR_TOKEN'
+ACCESS_KEY = 'ADD_YOUR_ACCESS_KEY'
+SECRET_KEY = 'ADD_YOUR_SECRET_KEY'
+PARTNER_ID = 'ADD_YOUR_AFFILIATE_ID'
+
+# FOR HOST AND REGION https://webservices.amazon.com/paapi5/documentation/common-request-parameters.html#host-and-region
+
+HOST = "ADD_YOUR_HOST"
+REGION = "ADD_YOUR_REGION"
